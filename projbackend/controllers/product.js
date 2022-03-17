0c03d1f80b09f39ce06bc5840f4d7e954480ed25
kumar-tushar <kumartushar0110@gmail.com>
2022-03-17 11:34:36 +0530

    .

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -75,3 +75,111 @@ exports.photo = (req, res, next) => {
     }
     next();
 };
+
+// delete controllers
+exports.deleteProduct = (req, res) => {
+    let product = req.product;
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Failed to delete the product"
+            });
+        }
+        res.json({
+            message: "Deletion was a success",
+            deletedProduct
+        });
+    });
+};
+
+// delete controllers
+exports.updateProduct = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, file) => {
+        if (err) {
+            return res.status(400).json({
+                error: "problem with image"
+            });
+        }
+
+        //updation code
+        let product = req.product;
+        product = _.extend(product, fields);
+
+        //handle file here
+        if (file.photo) {
+            if (file.photo.size > 3000000) {
+                return res.status(400).json({
+                    error: "File size too big!"
+                });
+            }
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.contentType = file.photo.type;
+        }
+        // console.log(product);
+
+        //save to the DB
+        product.save((err, product) => {
+            if (err) {
+                res.status(400).json({
+                    error: "Updation of product failed"
+                });
+            }
+            res.json(product);
+        });
+    });
+};
+
+//product listing
+
+exports.getAllProducts = (req, res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 8;
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+
+    Product.find()
+        .select("-photo")
+        .populate("category")
+        .sort([[sortBy, "asc"]])
+        .limit(limit)
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "NO product FOUND"
+                });
+            }
+            res.json(products);
+        });
+};
+
+exports.getAllUniqueCategories = (req, res) => {
+    Product.distinct("category", {}, (err, category) => {
+        if (err) {
+            return res.status(400).json({
+                error: "NO category found"
+            });
+        }
+        res.json(category);
+    });
+};
+
+exports.updateStock = (req, res, next) => {
+    let myOperations = req.body.order.products.map(prod => {
+        return {
+            updateOne: {
+                filter: { _id: prod._id },
+                update: { $inc: { stock: -prod.count, sold: +prod.count } }
+            }
+        };
+    });
+
+    Product.bulkWrite(myOperations, {}, (err, products) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Bulk operation failed"
+            });
+        }
+        next();
+    });
+};
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const {getProductById, createProduct, getProduct, photo
+const {
+    getProductById,
+    createProduct,
+    getProduct,
+    photo,
+    updateProduct,
+    deleteProduct,
+    getAllProducts,
+    getAllUniqueCategories
 } = require("../controllers/product");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
@@ -11,6 +19,7 @@ router.param("userId", getUserById);
 router.param("productId", getProductById);
 
 //all of actual routes
+//create route
 router.post(
     "/product/create/:userId",
     isSignedIn,
@@ -19,7 +28,31 @@ router.post(
     createProduct
 );
 
+// read routes
 router.get("/product/:productId", getProduct);
 router.get("/product/photo/:productId", photo);
+
+//delete route
+router.delete(
+    "/product/:productId/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    deleteProduct
+);
+
+//update route
+router.put(
+    "/product/:productId/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    updateProduct
+);
+
+//listing route
+router.get("/products", getAllProducts);
+
+router.get("/products/categories", getAllUniqueCategories);
 
 module.exports = router;
