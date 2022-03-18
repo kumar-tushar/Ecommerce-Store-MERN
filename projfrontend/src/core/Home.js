d53ef261a535be8179b810d9959c45cc2b19d0eb
kumar-tushar <kumartushar0110@gmail.com>
2022-03-18 18:59:21 +0530

    .

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
     console.log("API IS", API);
 
     return (
-        <Base title="Home Page">
+        <Base title="Home Page" description="Welcome to the Tshirt Store">
             <div className="row">
                 <div className="col-4">
                     <button className="btn btn-success">TEST</button>
---
+++ projfrontend/src/user/Signin.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Link } from "react-router-dom";
+
+const Signin = () => {
+    const signInForm = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form>
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" type="email" />
+                        </div>
+
+                        <div className="form-group">
+                            <label className="text-light">Password</label>
+                            <input className="form-control" type="password" />
+                        </div>
+                        <button className="btn btn-success btn-block">Submit</button>
+                    </form>
+                </div>
+            </div>
+        );
+    };
+
+    return (
+        <Base title="Sign In page" description="A page for user to sign in!">
+            {signInForm()}
+        </Base>
+    );
+};
+
+export default Signin;
