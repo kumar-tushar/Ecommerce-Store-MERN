e5ed6fcdbf69827b76017cd848c7674e912853c2
kumar-tushar <kumartushar0110@gmail.com>
2022-03-21 19:19:19 +0530

    .

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -3,7 +3,7 @@ import Menu from "./Menu";
 
 const Base = ({
                   title = "My Title",
-                  description = "My description",
+                  description = "My desription",
                   className = "bg-dark text-white p-4",
                   children
               }) => (
@@ -16,14 +16,14 @@ const Base = ({
             </div>
             <div className={className}>{children}</div>
         </div>
-        <footer className="footer bg-dark mt-5 py-5" >
+        <footer className="footer bg-dark mt-auto py-3">
             <div className="container-fluid bg-success text-white text-center py-3">
                 <h4>If you got any questions, feel free to reach out!</h4>
                 <button className="btn btn-warning btn-lg">Contact Us</button>
             </div>
             <div className="container">
         <span className="text-muted">
-          The Ultimate <span className="text-white">T-Shirts</span> Store
+          An Amazing <span className="text-white">MERN</span> Bootcamp
         </span>
             </div>
         </footer>
--- projfrontend/src/user/AdminDashBoard.js
+++ projfrontend/src/user/AdminDashBoard.js
@@ -19,6 +19,11 @@ const AdminDashBoard = () => {
                         </Link>
                     </li>
                     <li className="list-group-item">
+                        <Link to="/admin/categories" className="nav-link text-success">
+                            Manage Categories
+                        </Link>
+                    </li>
+                    <li className="list-group-item">
                         <Link to="/admin/create/product" className="nav-link text-success">
                             Create Product
                         </Link>
